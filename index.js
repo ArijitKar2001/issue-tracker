4765e9667407d4a8ab93579132ab6b3a18d16cf5
Arijit Karmakar <arijitkarmakarbnk@gmail.com>
2024-07-02 10:45:13 +0530

    $

--- index.js
+++ index.js
@@ -19,12 +19,11 @@ app.set("view engine", "ejs");
 app.set("views", "./views");
 
 // use express router
-// app.use("/", require("./routes"));
-// app.listen(port, (err) => {
-//   if (err) {
-//     console.log(`Error in running the server : ${err}`);
-//     return;
-//   }
-//   console.log(`Server is running on the port: ${port}`);
-// });
-module.exports.handler = serverless(app);
+app.use("/", require("./routes"));
+app.listen(port, (err) => {
+  if (err) {
+    console.log(`Error in running the server : ${err}`);
+    return;
+  }
+  console.log(`Server is running on the port: ${port}`);
+});
