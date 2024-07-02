1d4cbfc704800a2d6736611567a70386f2e52252
Arijit Karmakar <arijitkarmakarbnk@gmail.com>
2024-07-02 10:33:21 +0530

    *

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 // set up database
 const db = require("./config/mongoose");
+const serverless = require("serverless-http");
 const port = 8000;
 const app = express();
 
@@ -18,11 +19,12 @@ app.set("view engine", "ejs");
 app.set("views", "./views");
 
 // use express router
-app.use("/", require("./routes"));
-app.listen(port, (err) => {
-  if (err) {
-    console.log(`Error in running the server : ${err}`);
-    return;
-  }
-  console.log(`Server is running on the port: ${port}`);
-});
+// app.use("/", require("./routes"));
+// app.listen(port, (err) => {
+//   if (err) {
+//     console.log(`Error in running the server : ${err}`);
+//     return;
+//   }
+//   console.log(`Server is running on the port: ${port}`);
+// });
+module.exports.handler = serverless(app);
