005e4cd9bf6fae01205461faa21eb8a85ea143b5
Arijit Karmakar <arijitkarmakarbnk@gmail.com>
2024-07-02 10:26:35 +0530

    @

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.use("/", require("./routes"));
 app.listen(port, (err) => {
   if (err) {
     console.log(`Error in running the server : ${err}`);
+    return;
   }
   console.log(`Server is running on the port: ${port}`);
 });
